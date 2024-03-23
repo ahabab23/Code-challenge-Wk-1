018e357d8d5efd96f1a844f84297ac6c7ff488d9
Uwes Ahabab <uwesahabab@gmail.com>
2024-03-23 12:55:49 +0300

    Improve contents in Grade-Generator/script.js

--- Grade-Generator/script.js
+++ Grade-Generator/script.js
@@ -2,22 +2,36 @@ let button = document.querySelector(".button")
 let result = document.querySelector(".result");
 
 let grade = document.querySelector(".grade")
+function check() {
+    // Get the values of the HTML elements with the IDs javascript, css, python, react, and ruby
+    let js = document.querySelector("#javascript").value;
+    let css = document.querySelector("#css").value;
+    let python = document.querySelector("#python").value;
+    let react = document.querySelector("#react").value;
+    let ruby = document.querySelector("#ruby").value;
+
+    // Check if any of the input values are empty or out of range
+    if (js === '' || css === '' || python === '' || react === '' || ruby === '' ||
+        js < 0 || js > 100 || css < 0 || css > 100 || python < 0 || python > 100 ||
+        react < 0 || react > 100 || ruby < 0 || ruby > 100) {
+        // If it is, display a message in the result element
+        result.innerHTML = `<p>Please Enter A Valid Value Between 0 and 100</p>`
+    }
+}
 function calculate() {
     let js = document.querySelector("#javascript").value;
     let css = document.querySelector("#css").value;
     let python = document.querySelector("#python").value;
     let react = document.querySelector("#react").value;
     let ruby = document.querySelector("#ruby").value;
-    if (document.querySelector("input").value==''){
-        result.innerHTML=`<p>Please Enter A value</p>`
-    }
+    check()
     let sum = parseFloat(js) + parseFloat(css) + parseFloat(python) + parseFloat(react) + parseFloat(ruby);
     let average = (sum / 500) * 100;
     if (average > 79 && average <= 100) {
         grade.innerHTML = `Grade: A`
     } else if (average > 60 && average <= 79) {
         grade.innerHTML = `Grade: B`
-    }else if (average > 49 && average <= 60) {
+    } else if (average > 49 && average <= 60) {
         grade.innerHTML = `Grade: C`
     } else if (average > 40 && average <= 49) {
         grade.innerHTML = `Grade: D`
