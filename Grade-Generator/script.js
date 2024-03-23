7b112173e1e397abd91edc7d54ba12b3d748f04f
Uwes Ahabab <uwesahabab@gmail.com>
2024-03-23 15:52:05 +0300

    Corrects the problems with if ,else if condition

--- Grade-Generator/script.js
+++ Grade-Generator/script.js
@@ -8,6 +8,8 @@ function check() {
     let python = document.querySelector("#python").value;
     let react = document.querySelector("#react").value;
     let ruby = document.querySelector("#ruby").value;
+    let totalMarks = document.querySelector('.total_marks');
+    let percentage = document.querySelector('.Percentage')
     let sum = parseFloat(js) + parseFloat(css) + parseFloat(python) + parseFloat(react) + parseFloat(ruby);
     let average = (sum / 500) * 100;
 
@@ -16,22 +18,27 @@ function check() {
         js < 0 || js > 100 || css < 0 || css > 100 || python < 0 || python > 100 ||
         react < 0 || react > 100 || ruby < 0 || ruby > 100) {
         // If it is, display a message in the result element
-        result.innerHTML = `<p>Please Enter A Valid Value Between 0 and 100</p>`
-    } else if (average > 79 && average <= 100) {
-        grade.innerHTML = `Grade: A`
-    } else if (average > 60 && average <= 79) {
-        grade.innerHTML = `Grade: B`
-    } else if (average > 49 && average <= 60) {
-        grade.innerHTML = `Grade: C`
-    } else if (average > 40 && average <= 49) {
-        grade.innerHTML = `Grade: D`
+        grade.innerHTML = `<p>Please Enter A Valid Value Between 0 and 100</p>`
+        totalMarks.innerHTML = ``
+        percentage.innerHTML = ``
+
     } else {
-        grade.innerHTML = `Grade: E`
+        if (average > 79 && average <= 100) {
+            grade.innerHTML = `Grade: A`
+        } else if (average > 60 && average <= 79) {
+            grade.innerHTML = `Grade: B`
+        } else if (average > 49 && average <= 60) {
+            grade.innerHTML = `Grade: C`
+        } else if (average > 40 && average <= 49) {
+            grade.innerHTML = `Grade: D`
+        } else {
+            grade.innerHTML = `Grade: E`
+        }
+        totalMarks.innerHTML = `Total Marks : ${sum}/500`
+        percentage.innerHTML = `Percentage : ${Math.round(average)}%`
     }
-    let totalMarks = document.querySelector('.total_marks');
-    let percentage = document.querySelector('.Percentage')
-    totalMarks.innerHTML = `Total Marks : ${sum}/500`
-    percentage.innerHTML = `Percentage : ${Math.round(average)}%`
+
+
 }
 
 
